ef397b52a0de78f7df3037147b91f59de6dbdbe9
prohenriquelima <prohenriquelima@gmail.com>
2022-10-03 21:10:00 -0300

    added controller tax regime

---
+++ testportifolio/src/main/java/com/api/testportifolio/controllers/TaxRegimeController.java
@@ -0,0 +1,36 @@
+package com.api.testportifolio.controllers;
+
+import com.api.testportifolio.dtos.ClientRequestDto;
+import com.api.testportifolio.models.ClientModel;
+import com.api.testportifolio.models.TaxRegimeModel;
+import com.api.testportifolio.repositories.ClientRepository;
+import com.api.testportifolio.repositories.TaxRegimeRepository;
+import com.api.testportifolio.services.ClientService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/api/taxregime")
+@CrossOrigin(origins = "*")
+public class TaxRegimeController {
+
+    @Autowired
+    TaxRegimeRepository taxService;
+
+    @PostMapping
+    public ResponseEntity<Object> saveTaxRegime(@RequestBody TaxRegimeModel taxRegimeModel){
+        return ResponseEntity.status(HttpStatus.OK).body(taxService.save(taxRegimeModel));
+    }
+
+    @GetMapping("/all")
+    public  ResponseEntity<Object> getAllCustomers(){
+        return ResponseEntity.status(HttpStatus.OK).body(taxService.findAll());
+    }
+
+
+}
